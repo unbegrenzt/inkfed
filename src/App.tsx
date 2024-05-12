aeea3eef7dee3ddfac1aaa5a3ef3ff6b986d1cbc
unbegrenzt <jmoralescenteno@gmail.com>
2024-05-11 21:59:47 -0600

    feat: :sparkles: deploy github action dist folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/inkfed.svg'
 import './App.css'
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 
 function App() {
   const [count, setCount] = useState(0)
-const firebaseConfig = {
-  apiKey: "AIzaSyD9r0VsaTFQ1MFQIDGYvXAfI-HFeScWM6Y",
-  authDomain: "inkfed-f18a9.firebaseapp.com",
-  projectId: "inkfed-f18a9",
-  storageBucket: "inkfed-f18a9.appspot.com",
-  messagingSenderId: "652348663875",
-  appId: "1:652348663875:web:9aaad37e7b0bb4e7c2ccf6",
-  measurementId: "G-S4P8ZRS9VX"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD9r0VsaTFQ1MFQIDGYvXAfI-HFeScWM6Y",
+//   authDomain: "inkfed-f18a9.firebaseapp.com",
+//   projectId: "inkfed-f18a9",
+//   storageBucket: "inkfed-f18a9.appspot.com",
+//   messagingSenderId: "652348663875",
+//   appId: "1:652348663875:web:9aaad37e7b0bb4e7c2ccf6",
+//   measurementId: "G-S4P8ZRS9VX"
+// };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
   return (
     <>
